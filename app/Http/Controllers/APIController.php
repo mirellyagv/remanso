0515b9bdc5685ddb012cc3406c4f406a81767c24
bgalvanguerrero <bgalvanguerrero@gmail.com>
2023-06-13 22:53:42 -0500

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -175,6 +175,35 @@ class APIController extends Controller
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
+
+    public function editarProspecto(Request $request)
+    {
+        $client = new Client();
+        $data = json_encode($request['prospecto']);
+        $header = [
+            'Content-Type' => 'application/json'
+        ];
+
+        try {
+
+            $request = new \GuzzleHttp\Psr7\Request('PUT', 'https://webapiportalcontratoremanso.azurewebsites.net/api/Prospecto/ActualizarProspecto/20396900719', $header, $data);
+            $promise = $client->sendAsync($request)->then(function ($response) {
+                echo  $response->getBody();
+                $code = $response->getStatusCode();
+                $reason = $response->getReasonPhrase();
+
+                return response()->json(['status' => $code, 'mensaje' => $reason]);
+            });
+            
+            $promise->wait();
+            $mensaje= 'Actualizado';
+            return $mensaje;
+        } catch (\Exception $e) {
+            // Manejo de errores en caso de que la petición falle
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
     public function guardaObservacion(Request $request)
     {
         $client = new Client();
